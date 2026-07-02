686d7f3781c5127c7eb04af94279a7dde3158a76
xin234579 <eexxnn7@gmail.com>
2026-07-03 00:16:09 +0800

    Update only_a.c

--- Exams Practice/Exam00/0-only_a/only_a.c
+++ Exams Practice/Exam00/0-only_a/only_a.c
@@ -6,15 +6,3 @@ int		main(void)
 	// Show 'a' on the standard output
 	write(1, "a", 1);
 }
-
-or
-#include <unistd.h>
-
-int main(void)
-{
-    // 1. Create a single character variable using single quotes
-    char letter = 'a';
-
-    // 2. Use the '&' symbol to pass the address of that variable
-    write(1, &letter, 1);
-}
